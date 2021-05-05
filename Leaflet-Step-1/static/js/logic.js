75116a340fd6e052c79bf41ea19309626318e91c
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-05-05 11:04:50 -0500

    add directions

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,15 @@
+console.log("reading logic.js")
+
+// pull data from USGS JSON at https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
+// create map with leaflet to plot all earthquakes from data set
+
+// adjust datamarkers to reflect magnitude of earthquake by size and depth of earthquake by color
+// color should be larger for higher magnitude and darker for deeper depth (third coordinate for each earthquake)
+
+// add a legend with the color scheme 
+
+// PART TWO
+// plot second data set on map from https://github.com/fraxen/tectonicplates
+// add number of base maps to choose from (satellite, grayscale, outdoors)
+// add layer controls to our mapEPR wit
