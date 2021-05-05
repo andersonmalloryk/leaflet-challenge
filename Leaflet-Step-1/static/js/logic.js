caba7c8ce180d2d9b90c8944f18900f0b520c9bf
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-05-05 15:50:28 -0500

    map and earthquake locations display with radius linked to magnitude

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,49 +1,66 @@
 console.log("reading logic.js")
 
-// Create a map object
-var myMap = L.map("mapid", {
-  center: [15.5994, -28.6731],
-  zoom: 3
-});
+function createMap(earthquakeLocations) {
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+  var baseMaps = {
+    "Light Map": lightmap
+  }
 
-//function createMarkers(response) {
+  var overlayMaps = {
+    "Earthquake Locations": earthquakeLocations
+  }
 
-    // // Pull the "stations" property off of response.data
-    // var stations = response.data.stations;
-  
-    // // Initialize an array to hold bike markers
-    // var bikeMarkers = [];
-  
-    // // Loop through the stations array
-    // for (var index = 0; index < stations.length; index++) {
-    //   var station = stations[index];
-  
-    //   // For each station, create a marker and bind a popup with the station's name
-    //   var bikeMarker = L.marker([station.lat, station.lon])
-    //     .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-  
-    //   // Add the marker to the bikeMarkers array
-    //   bikeMarkers.push(bikeMarker);
-    // }
-  
-    // // Create a layer group made from the bike markers array, pass it into the createMap function
-    // createMap(L.layerGroup(bikeMarkers));
-  //}
-  
-  
-  // Perform an API call to the USGS JSON to get earthquake information. Call createMarkers when complete
-  //d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+  var myMap = L.map("mapid", {
+    center: [15.5994, -28.6731],
+    zoom: 2,
+    layers: [lightmap, earthquakeLocations]
+  });
 
-// create map with leaflet to plot all earthquakes from data set
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
+
+function createCircles(response) {
+
+    // Pull the "stations" property off of response.data
+    var quakes = response.features;
+    // console.log(quakes[0].geometry.coordinates)
+  
+    // Initialize an array to hold bike markers
+    var quakeLocations = [];
+  
+    // Loop through the stations array
+    for (var index = 0; index < quakes.length; index++) {
+
+      var quake = quakes[index];
+      //console.log(quake.geometry.coordinates[0], quake.geometry.coordinates[1])
+  
+      // For each earthquake, create a marker and bind a popup with the station's name
+      var quakeCircle = L.circle([quake.geometry.coordinates[0], quake.geometry.coordinates[1]], 
+        {fillOpacity: 0.75,
+        fillColor: "blue",
+        radius: quake.properties.mag * 50000
+      }).bindPopup("<h3>" + quake.properties.place + "<h3><h3> Magnitude: " + quake.properties.mag + "</h3>");
+  
+      // Add circles to the array
+      quakeLocations.push(quakeCircle);
+    }
+  
+    // Create a layer group made from the quake location array, pass it into the createMap function
+    createMap(L.layerGroup(quakeLocations));
+}
+
+// Perform an API call to the USGS JSON to get earthquake information. Call createMarkers when complete
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createCircles);
 
 // adjust datamarkers to reflect magnitude of earthquake by size and depth of earthquake by color
 // color should be larger for higher magnitude and darker for deeper depth (third coordinate for each earthquake)
