99bdcf26f46f1739979d787d961ab3b73a711731
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-05-05 13:10:40 -0500

    added code from city bike example

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiYW5kZXJzb25tYWxsb3J5ayIsImEiOiJja25ycWJ3a2owb2FvMzFvOXpycXFjeTVzIn0.AORoA1s3I1XPTokvdqXcCQ";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,48 @@
 console.log("reading logic.js")
 
-// pull data from USGS JSON at https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+// Create map object
+var myMap = L.map("map", {
+    center: [39.588, 2.935],
+    zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+function createMarkers(response) {
+
+    // // Pull the "stations" property off of response.data
+    // var stations = response.data.stations;
+  
+    // // Initialize an array to hold bike markers
+    // var bikeMarkers = [];
+  
+    // // Loop through the stations array
+    // for (var index = 0; index < stations.length; index++) {
+    //   var station = stations[index];
+  
+    //   // For each station, create a marker and bind a popup with the station's name
+    //   var bikeMarker = L.marker([station.lat, station.lon])
+    //     .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+  
+    //   // Add the marker to the bikeMarkers array
+    //   bikeMarkers.push(bikeMarker);
+    // }
+  
+    // // Create a layer group made from the bike markers array, pass it into the createMap function
+    // createMap(L.layerGroup(bikeMarkers));
+  }
+  
+  
+  // Perform an API call to the USGS JSON to get earthquake information. Call createMarkers when complete
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
 
 // create map with leaflet to plot all earthquakes from data set
 
@@ -12,4 +54,4 @@ console.log("reading logic.js")
 // PART TWO
 // plot second data set on map from https://github.com/fraxen/tectonicplates
 // add number of base maps to choose from (satellite, grayscale, outdoors)
-// add layer controls to our mapEPR wit
+// add layer controls map
