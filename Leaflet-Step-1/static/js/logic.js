1435c2eb04f50157a7dc7b8272eda8aeef01f813
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-05-05 13:32:31 -0500

    map loads

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,12 +1,11 @@
 console.log("reading logic.js")
 
-// Create map object
-var myMap = L.map("map", {
-    center: [39.588, 2.935],
-    zoom: 5
+// Create a map object
+var myMap = L.map("mapid", {
+  center: [15.5994, -28.6731],
+  zoom: 3
 });
 
-// Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -16,7 +15,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-function createMarkers(response) {
+//function createMarkers(response) {
 
     // // Pull the "stations" property off of response.data
     // var stations = response.data.stations;
@@ -38,11 +37,11 @@ function createMarkers(response) {
   
     // // Create a layer group made from the bike markers array, pass it into the createMap function
     // createMap(L.layerGroup(bikeMarkers));
-  }
+  //}
   
   
   // Perform an API call to the USGS JSON to get earthquake information. Call createMarkers when complete
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+  //d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
 
 // create map with leaflet to plot all earthquakes from data set
 
